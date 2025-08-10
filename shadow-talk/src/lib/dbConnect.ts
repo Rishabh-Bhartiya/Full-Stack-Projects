9b708fa01ec1b65baf64c8a3d149f173985f705c
Rishabh Bhartiya <rishabh7rz@gmail.com>
2025-08-11 02:18:12 +0530

    Database Connection

---
+++ shadow-talk/src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected) {
+        console.log("Already connected to the database");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB => ",db);
+        console.log("DB.CONNECTIONS => ", db.connections);
+        
+
+        console.log("DataBase Connected Successfully");        
+    } catch (error) {
+        console.log("DataBase Connection Failed", error);
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
